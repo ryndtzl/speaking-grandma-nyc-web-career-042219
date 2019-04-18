4911fef5e9106a133fb703df985ae475306cbe40
Ryan Ditzel <ryand370@gmail.com>
2019-04-18 00:22:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(greeting)
-  if greeting == greeting.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif greeting == "I LOVE YOU GRANDMA!"
+  if greeting == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+    
+    greeting == greeting.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif 
   else  greeting == greeting.downcase
     return "HUH?! SPEAK UP, SONNY!"
 end
