f0beb836080123b4795daf046e232dea83bc60a2
Ryan Ditzel <ryand370@gmail.com>
2019-04-18 00:23:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,8 @@
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    
-    greeting == greeting.upcase
+  elsif greeting == greeting.upcase
     return "NO, NOT SINCE 1938!"
-  elsif 
   else  greeting == greeting.downcase
     return "HUH?! SPEAK UP, SONNY!"
 end
